9fa7f26bb56be1fd7d29a76880a1e03e28fedb1d
Ingrid <97858991+Shun1124@users.noreply.github.com>
2024-11-07 09:31:04 -0800

    Update jsonUtils.cpp

--- Monitor/jsonUtils.cpp
+++ Monitor/jsonUtils.cpp
@@ -7,17 +7,17 @@
 
 namespace JsonUtils {
 
-QList<RegistryKey*> readKeysFromJson(const QString &filePath) {
+QList<RegistryKey*> readKeysFromJson(const QString &filePath) { // Function to read registry keys from a JSON file and return them as a list
     QFile file(filePath);
     QList<RegistryKey*> registryKeys;
 
-    if (!file.open(QIODevice::ReadOnly)) {
-        qWarning() << "Could not open file:" << filePath;
-        return registryKeys;
+    if (!file.open(QIODevice::ReadOnly)) { // Attempts to open the file in read-only mode
+        qWarning() << "Could not open file:" << filePath; // Logs a warning if the file can't be opened
+        return registryKeys; // Returns an empty list if the file cannot be opened
     }
 
     QJsonDocument doc = QJsonDocument::fromJson(file.readAll());
-    QJsonArray keysArray = doc.array();
+    QJsonArray keysArray = doc.array(); // Retrieves the JSON array from the document
 
     for (const QJsonValue &value : keysArray) {
         QJsonObject obj = value.toObject();
@@ -26,11 +26,11 @@ QList<RegistryKey*> readKeysFromJson(const QString &filePath) {
         QString valueName = obj["valueName"].toString();
         bool isCritical = obj["isCritical"].toBool();
 
-        registryKeys.append(new RegistryKey(hive, keyPath, valueName, isCritical));
+        registryKeys.append(new RegistryKey(hive, keyPath, valueName, isCritical)); // Creates a new RegistryKey object and appends it to the list
     }
 
     qDebug() << "Registry Keys model size:" << registryKeys.size();
     return registryKeys;
 }
 
-} // namespace JsonUtils
+} // namespace JsonUtils // Closes the JsonUtils namespace
