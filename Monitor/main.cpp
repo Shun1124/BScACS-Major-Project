7b8a6bce49a4a06c8c213ea3e07c8f0d3b93a5a4
Ingrid <97858991+Shun1124@users.noreply.github.com>
2024-11-07 09:30:42 -0800

    Update main.cpp

--- Monitor/main.cpp
+++ Monitor/main.cpp
@@ -5,13 +5,13 @@
 #include "monitoring.h"
 #include "settings.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { // Main entry point for the application
     Aws::SDKOptions options;
-    Aws::InitAPI(options);  // Initialize the SDK
+    Aws::InitAPI(options);
 
-    QGuiApplication app(argc, argv);
+    QGuiApplication app(argc, argv); //
 
-    QQmlApplicationEngine engine;
+    QQmlApplicationEngine engine; // Creates the QML application engine for loading QML files
     Settings settings;
     Monitoring monitoring(&settings);
 
@@ -20,12 +20,12 @@ int main(int argc, char *argv[]) {
     engine.rootContext()->setContextProperty("Monitoring", &monitoring);
     engine.load(QUrl(QStringLiteral("qrc:/qt/qml/Monitor/Main.qml")));
 
-    QObject::connect(&monitoring, &Monitoring::logMessage, &engine, [&engine](const QString &message) {
+    QObject::connect(&monitoring, &Monitoring::logMessage, &engine, [&engine](const QString &message) { // Connects the logMessage signal from Monitoring to a lambda function
         QObject *rootObject = engine.rootObjects().first();
         QMetaObject::invokeMethod(rootObject, "addLog", Q_ARG(QVariant, message));
     });
 
-    int result = app.exec();
-    Aws::ShutdownAPI(options);  // Shutdown the SDK
+    int result = app.exec(); // Starts the event loop and waits until the application is closed
+    Aws::ShutdownAPI(options);
     return result;
 }
