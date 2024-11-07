0d5aa45694dee9cce4059112342b9ec59e89cddc
Ingrid <97858991+Shun1124@users.noreply.github.com>
2024-11-07 09:28:08 -0800

    Update rollback.h

--- Monitor/rollback.h
+++ Monitor/rollback.h
@@ -8,16 +8,16 @@ class Rollback : public QObject {
     Q_OBJECT
 
 public:
-    explicit Rollback(QObject *parent = nullptr);
+    explicit Rollback(QObject *parent = nullptr); // Constructor for Rollback, with an optional parent parameter
     void registerKeyForRollback(RegistryKey* key);
     void rollbackIfNeeded(RegistryKey* key);
-    void cancelRollback(RegistryKey* key);  // New method to cancel rollback
+    void cancelRollback(RegistryKey* key);
 
 signals:
     void rollbackPerformed(QString keyName);
 
 private:
-    void restorePreviousValue(RegistryKey* key);
+    void restorePreviousValue(RegistryKey* key); //
 };
 
-#endif // ROLLBACK_H
+#endif
