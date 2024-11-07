52e780ba05f4480ecac395decb7d6a5c306e694a
Ingrid <97858991+Shun1124@users.noreply.github.com>
2024-11-07 09:29:43 -0800

    Update registryKey.h

--- Monitor/registryKey.h
+++ Monitor/registryKey.h
@@ -7,11 +7,11 @@
 
 class RegistryKey : public QObject {
     Q_OBJECT
-    Q_PROPERTY(QString displayText READ displayText NOTIFY displayTextChanged)
-    Q_PROPERTY(bool isCritical READ isCritical WRITE setCritical NOTIFY isCriticalChanged)
+    Q_PROPERTY(QString displayText READ displayText NOTIFY displayTextChanged) // Exposes displayText property to QML, with a notification signal
+    Q_PROPERTY(bool isCritical READ isCritical WRITE setCritical NOTIFY isCriticalChanged) // Exposes isCritical property to QML, with a read, write, and notification
 
 public:
-    explicit RegistryKey(const QString &hive, const QString &keyPath, const QString &valueName, bool isCritical, QObject *parent = nullptr);
+    explicit RegistryKey(const QString &hive, const QString &keyPath, const QString &valueName, bool isCritical, QObject *parent = nullptr); // Constructor for RegistryKey
 
     bool isCritical() const;
     void setCritical(bool critical);
@@ -31,7 +31,6 @@ public:
     QString keyPath() const { return m_keyPath; }
     QString valueName() const { return m_valueName; }
 
-    // New methods
     bool isRollbackCancelled() const;
     void setRollbackCancelled(bool cancelled);
 
@@ -54,13 +53,9 @@ private:
     QSettings *m_settings;
     QString m_newValue;
     int m_changeCount = 0;
-
-    // New property to track if rollback is cancelled
     bool m_rollbackCancelled = false;
-
     void updateDisplayText();
     QString readCurrentValue() const;
-
 };
 
-#endif // REGISTRYKEY_H
+#endif
